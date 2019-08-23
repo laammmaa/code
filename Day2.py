2ad6efbf684a4c83aa9704a66e131b09f2911bce
laammmaa <52979649+laammmaa@users.noreply.github.com>
2019-08-23 03:21:03 +0300

    #100DaysOfCode

---
+++ Day2.py
@@ -0,0 +1,15 @@
+# this is a comment
+print("Hello, World!")
+print("Hello, World!")  # this is a comment
+# print("Hello, World!")
+print("Cheers, Mate!")
+# this is ac comment
+# written in
+# more than just one line
+print("Hello, World!")
+"""
+this is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
---
+++ Day3.py
@@ -0,0 +1,40 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+print('\n')
+x = 4  # x is of type int
+x = "sally"  # x is now of type str
+print(x)
+print('\n')
+x = "John"
+print(x)
+# double qoutes are the same as single qoutes:
+x = 'John'
+print(x)
+print('\n')
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+print('\n')
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+print('\n')
+x = "awesome"
+print("Python is " + x)
+print('\n')
+x = "Python is "
+y = "awesome"
+z = x + y
+print(z)
+print('\n')
+x = 5
+y = 10
+print(x + y)
+print('\n')
+x = 5
+y = "John"
+print(x + y)
