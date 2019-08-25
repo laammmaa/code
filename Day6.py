e5d8015538528ccc64bea24a2eb304305d800e64
laammmaa <52979649+laammmaa@users.noreply.github.com>
2019-08-25 17:49:47 +0300

    #100DaysOfCode

---
+++ Day6.py
@@ -0,0 +1,23 @@
+# Example 1
+x = int(1)  # x will be 1
+y = int(2.8)  # y will be 2
+z = int("3")  # z will be 3
+print(x)
+print(y)
+print(z)
+# Example 2
+x = float(1)  # x will be 1.0
+y = float(2.8)  # y will be 2.8
+z = float("3")  # z will be 3.0
+w = float("4.2")  # will be 4.2
+print(x)
+print(y)
+print(z)
+print(w)
+# Example 3
+x = str("sl")  # x will be 'sl'
+y = str(2)  # y will be '2'
+z = str(3.0)  # z will be'3.0'
+print(x)
+print(y)
+print(z)
