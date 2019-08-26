50f44ad78326b80d163a935a3575a614f96e18fb
laammmaa <52979649+laammmaa@users.noreply.github.com>
2019-08-26 18:45:30 +0300

    #100DaysOfCode

---
+++ Day8.py
@@ -0,0 +1,18 @@
+# Example 1
+a = "Hello, World!"
+print(a.strip())  # returns "Hello, World!"
+# Example 2
+a = "Hello, World!"
+print(len(a))
+# Example 3
+a = "Hello, World!"
+print(a.lower())
+# Example 4
+a = "Hello, World!"
+print(a.upper())
+# Example 5
+a = "Hello, World!"
+print(a.replace("H", "J"))
+# Example 6
+a = "Hello, World!"
+print(a.split(","))  # returns ['Hello, 'World!']
