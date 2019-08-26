c23265f42794c8350d5706f294efadcc20d56175
laammmaa <52979649+laammmaa@users.noreply.github.com>
2019-08-26 18:35:21 +0300

    #100DaysOfCode

---
+++ Day7.py
@@ -0,0 +1,22 @@
+# Example 1
+print("Hello")
+print('Hello')
+print('\n')
+# Example 2
+a = "Hello"
+print(a)
+print('\n')
+# Example 3
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+print('\n')
+# Example 4
+a = "Hello, World!"
+print(a[1])
+print('\n')
+# Example 5
+b = "Hello, World!"
+print(b[2:5])
