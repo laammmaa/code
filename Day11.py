97f9925a6b1d3a37b9c30f325203693676f133b8
laammmaa <52979649+laammmaa@users.noreply.github.com>
2019-08-28 07:17:49 +0300

    #100DaysOfCode

--- Day11.py
+++ Day11.py
@@ -1,10 +1,13 @@
 x = 5
 print(x > 3 or x < 4)
+print('\n')
 x = [" apple", "banana"]
 y = [" apple", "banana"]
 z = x
 print(x is not z)
 print(x is not y)
 print(x != y)
+print('\n')
+
 x = [" apple", "banana"]
 print("banana" in x)
