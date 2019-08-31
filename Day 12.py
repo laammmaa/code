5100d444e3b22b72fde8889ed493106028165855
laammmaa <52979649+laammmaa@users.noreply.github.com>
2019-08-31 05:32:18 +0300

    #100DaysOfCode

---
+++ Day 12.py
@@ -0,0 +1,12 @@
+print("please, I want {} sandwishes and {} donutes ")
+print('\n')
+a = "please, I want {} sandwishes and {} donutes"
+print(a.replace("I", "we"))
+print('\n')
+a = 5
+b = 7
+myorder = "please, I want {} sandwishes and {} donutes"
+print(myorder.format(a, b))
+print('\n')
+c = "please, I want {} sandwishes and {} donutes "
+print(c.replace("a", "A"))
