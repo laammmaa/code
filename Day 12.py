005f43a0589420884b95eca4ba488b10e8307771
laammmaa <52979649+laammmaa@users.noreply.github.com>
2019-08-31 17:05:07 +0300

    #100DaysOfCode

--- Day 12.py
+++ Day 12.py
@@ -1,12 +1,6 @@
-print("please, I want {} sandwishes and {} donutes ")
-print('\n')
-a = "please, I want {} sandwishes and {} donutes"
-print(a.replace("I", "we"))
-print('\n')
-a = 5
-b = 7
-myorder = "please, I want {} sandwishes and {} donutes"
-print(myorder.format(a, b))
-print('\n')
-c = "please, I want {} sandwishes and {} donutes "
-print(c.replace("a", "A"))
+S = "please, I want {} sandwishes and {} donutes"
+# Replacing "I" to "We" using replace()
+# Adding 5 and 7 to the sentence using format()
+# Replacing any "a" to "A" using replace()
+new_s = S.format(5, 7).replace("I", "We").replace("a", "A", len(S))
+print(new_s)
