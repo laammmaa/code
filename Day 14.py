bef085d5df66a8d4932dacd89aa6c7993c793408
laammmaa <52979649+laammmaa@users.noreply.github.com>
2019-09-02 01:17:24 +0300

    #100DaysOfCode

---
+++ Day 14.py
@@ -0,0 +1,17 @@
+thislist = ["A", "B", "C", "D", "E"]
+print(thislist[1:3])
+thislist = ["L", "A", "M", "A"]
+print(thislist[0:2])
+thislist = ["apple", "banana", "cherry"]
+if "apple" in thislist:
+    print("Yes, 'apple' is in the fruits list")
+thislist = ["Python"] * 4
+print(thislist)
+thislist1 = ["Ahmad", "Khalid", "Omar"]
+thislist2 = ["Sara", "Hind", "Batool"]
+thislist3 = thislist1 + thislist2
+print(thislist3)
+newlist1 = [1, 2, 3, ]
+newlist2 = [1.1, 1.2, 1.3]
+newlist3 = newlist1 + newlist2
+print(newlist3)
