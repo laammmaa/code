656de21c3af171bbac01f8fa564f539c72d342d2
laammmaa <52979649+laammmaa@users.noreply.github.com>
2019-09-01 22:36:10 +0300

    #100DaysOfCode

---
+++ Day 13.py
@@ -0,0 +1,31 @@
+S = []
+print(S)
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+thislist = ["apple", "banana", "cherry", 1, 2, 3]
+print(thislist)
+thislist = [1.5, 1.6, 1.7]
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
+print("------")
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+    print(x)
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+del thislist
+thislist = ["A", "B", "C", "D", "E"]
+print(thislist)
+thislist = ["A", "B", "C", "D", "E"]
+del thislist[0]
+del thislist[1]
+print(thislist)
