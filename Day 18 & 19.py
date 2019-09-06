14cc8191ed033d35e06f28b68fe593194288f15e
laammmaa <52979649+laammmaa@users.noreply.github.com>
2019-09-06 12:36:41 +0300

    #100DaysOfCode

---
+++ Day 18 & 19.py
@@ -0,0 +1,13 @@
+Numbers = [6, 8, 5, 9, 10, 4, 1]
+Numbers.sort()
+print(Numbers)
+Numbers.reverse()
+print(Numbers)
+print(len(Numbers))
+Numbers.clear()
+print(Numbers)
+programming_language = ("jave", "python", "swift")
+if "python" in programming_language:
+    print("Yes, python in programming languages")
+else:
+    print("No, it's not in programming languages")
