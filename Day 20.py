bcbe2ed5e3f406f549f03ddfb8f9c3d86eb2ccb3
laammmaa <52979649+laammmaa@users.noreply.github.com>
2019-09-08 20:26:05 +0300

    #100DaysOfCode

---
+++ Day 20.py
@@ -0,0 +1,17 @@
+thisset = {}
+print(thisset)
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+thisset = {"Ahmad", "Ahmad", 1, 2, 1, 5}
+print(thisset)
+thisset = {"apple", "banana", "cherry"}
+for x in thisset:
+    print(x)
+thisset = {"apple", "banana", "cherry"}
+print("banana" in thisset)
+thisset = {"apple", "banana", "cherry"}
+thisset.add("orange")
+print(thisset)
+thisset = {"apple", "banana", "cherry"}
+thisset.update(["orange", "mango", "grapes"])
+print(thisset)
