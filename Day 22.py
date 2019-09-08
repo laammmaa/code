dd968b7c0d27731039a8a336b13780407882f96c
laammmaa <52979649+laammmaa@users.noreply.github.com>
2019-09-08 21:02:49 +0300

    #100DaysOfCode

---
+++ Day 22.py
@@ -0,0 +1,63 @@
+thisdict = {}
+print(thisdict)
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(thisdict)
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+x = thisdict["model"]
+print(x)
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+x = thisdict.get("model")
+print(x)
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+thisdict["year"] = 2018
+print(thisdict)
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+for x in thisdict:
+    print(x)
+print('\n')
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+for x in thisdict:
+    print(thisdict[x])
+print('\n')
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+for x in thisdict.values():
+    print(x)
+thisdict = {"brand": "Ford", "model": "Mustang", "year": 1964}
+print(thisdict.values())
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+for x, y in thisdict.items():
+    print(x, y)
+thisdict = {"brand": "Ford", "model": "Mustang", "year": 1964}
+print(thisdict.items())
