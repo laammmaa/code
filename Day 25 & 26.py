72de64243d7cd3c70a656b9752ba1bf76f5827d4
laammmaa <52979649+laammmaa@users.noreply.github.com>
2019-09-14 00:30:27 +0300

    #100DaysOfCode

---
+++ Day 25 & 26.py
@@ -0,0 +1,16 @@
+# Q1
+S1 = {1, 3, 5, 7, 8}
+print(S1)
+S1.add(4)
+S1.add(8)
+S1.add(12)
+print(S1)
+S1.remove(8)
+print(S1)
+S1.clear()
+print(S1)
+# Q2
+dic = dict(name="pigeon", type="bird", skin_cover="feathers")
+print("type: ", dic['type'])
+dic['skin_cover'] = "none"
+print(dic)
