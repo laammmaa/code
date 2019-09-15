25c898dd1236effdbe0d9510c93ebcbe56c82709
laammmaa <52979649+laammmaa@users.noreply.github.com>
2019-09-15 21:15:27 +0300

    #100DaysOfCode

---
+++ Day 29.py
@@ -0,0 +1,22 @@
+fruits = ["apples", "banana", "cherry"]
+for x in fruits:
+    print(x)
+for x in "banana":
+    print(x)
+fruits = ["apples", "banana", "cherry"]
+for x in fruits:
+    print(x)
+    if x == "banana":
+        break
+print('\n')
+fruits = ["apples", "banana", "cherry"]
+for x in fruits:
+    if x == "banana":
+        break
+    print(x)
+print('\n')
+fruits = ["apples", "banana", "cherry"]
+for x in fruits:
+    if x == "banana":
+        continue
+    print(x)
