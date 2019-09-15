a4f1afc12276c68a798f22f2978f4ea302edaa6a
laammmaa <52979649+laammmaa@users.noreply.github.com>
2019-09-15 21:22:10 +0300

    #100DaysOfCode

---
+++ Day 30.py
@@ -0,0 +1,16 @@
+for x in range(2, 6):
+    print(x)
+print('\n')
+for x in range(2, 30, 3):
+    print(x)
+print('\n')
+for x in range(6):
+    print(x)
+else:
+    print("finally finished")
+print('\n')
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+for x in adj:
+    for y in fruits:
+        print(x, y)
