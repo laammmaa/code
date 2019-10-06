fd53e0e57f72f832878db12e2471c22cd37e619b
laammmaa <52979649+laammmaa@users.noreply.github.com>
2019-10-06 07:31:10 +0300

    #100DaysOfCode

---
+++ mymodule.py
@@ -0,0 +1,16 @@
+def greeting(name):
+    print("Hello," + name)
+
+
+import mymodule
+
+mymodule.greeting("Jonathan")
+person1 = {
+    "name": "John",
+    "age": 36,
+    "country ": "Norway"
+}
+import mymodule
+
+a = mymodule.person1["age"]
+print(a)
